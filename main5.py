0dd85894f56f002118654249cd44e8b61e7762c3
dgnytrnga <dgnytrnga@gmail.com>
2023-08-24 13:00:11 +0300

    Auto Message pyautogui

--- main5.py
+++ main5.py
@@ -1,12 +1,24 @@
 import pyautogui
 import time
 
-time.sleep(10)
+def send_Message(message):
+    try:
+        pyautogui.write(message)
+        pyautogui.press('enter')
+    except Exception as e:
+        print("Hata oluştu:", e)
 
-def mesaj():
-    pyautogui.write("Bebecim")
-    pyautogui.press('enter')
+def main():
+    try:
+        # It will start functioning 5 seconds after you run the code. You can modify it.
+        time.sleep(5)
 
-while True:
-    mesaj()
-    time.sleep(0)
+        message = input("Göndermek istediğiniz mesajı girin: ")
+        while True:
+            send_Message(message)
+            time.sleep(0)  # You can modify the time interval between messages.
+    except KeyboardInterrupt:
+        print("\nUygulama kullanıcı tarafından durduruldu.")
+
+if __name__ == "__main__":
+    main()
